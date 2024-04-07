2fd37846c0d0225c38c22b1ae65ec706852a4cc9
Bolotov Aleksandr <aleksandr.bolotov@osinit.com>
2024-04-07 14:59:03 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Александр Болотов on 07.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
